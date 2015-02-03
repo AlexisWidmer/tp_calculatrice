f157cce21ddc43b55a6d89802f71400f05ff48c4
Alexis Widmer <alexis.widmer@bts-malraux.net>
2015-02-03 11:48:37 +0100

    Premier push avec commentaires selon Doxygen et code complet sur plusieurs feuilles de code

---
+++ calculs/multiplication.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<stdint.h>
+using namespace std;
+
+/// \brief Multiplication de nombres int32
+///
+/// La fonction multiplication n'est pas la meme suivant les parametres (leur type)
+/// On appelle ca une fonction polymorphe
+///
+/// \param oppA est un entier 32 bits
+/// \param oppB est un entier 32 bits
+/// \return La multiplication des 2 entiers 32 bits donnes en parametres
+/// \sa multiplication(int64_t oppA. int64_t oppB). multiplication(float oppA. float oppB). multiplication(double oppA. double oppB).
+///
+
+int32_t multiplication(int32_t oppA, int32_t oppB)
+{
+        return (oppA*oppB);
+}
+
+///
+/// \brief Multiplication de nombres int64
+///
+/// La fonction multiplication n'est pas la meme suivant les parametres (leur type)
+/// On appelle ca une fonction polymorphe
+///
+/// \param oppA est un entier 64 bits
+/// \param oppB est un entier 64 bits
+/// \return La multiplication des 2 entiers 64 bits donnes en parametres
+/// \sa multiplication(int32_t oppA. int32_t oppB). multiplication(float oppA. float oppB). multiplication(double oppA. double oppB).
+///
+
+int64_t multiplication(int64_t oppA,int64_t oppB)
+{
+        return (oppA*oppB);
+}
+
+///
+/// \brief Multiplication de nombres float
+///
+/// La fonction multiplication n'est pas la meme suivant les parametres (leur type)
+/// On appelle ca une fonction polymorphe
+///
+/// \param oppA est un float
+/// \param oppB est un float
+/// \return La multiplication des 2 float donnes en parametres
+/// \sa multiplication(int32_t oppA. int32_t oppB). multiplication(int64_t oppA. int64_t oppB). multiplication(double oppA. double oppB).
+///
+
+float multiplication(float oppA, float oppB)
+{
+        return (oppA*oppB);
+}
+
+///
+/// \brief Multiplication de nombres double
+///
+/// La fonction multiplication n'est pas la meme suivant les parametres (leur type)
+/// On appelle ca une fonction polymorphe
+///
+/// \param oppA est un double
+/// \param oppB est un double
+/// \return La multiplication des 2 double donnes en parametres
+/// \sa multiplication(int32_t oppA. int32_t oppB). multiplication(int64_t oppA. int64_t oppB). multiplication(float oppA. float oppB).
+///
+
+double multiplication(double oppA, double oppB)
+{
+        return (oppA*oppB);
+}
+
